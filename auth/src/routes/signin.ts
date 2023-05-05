8d3cb2fedfc1febb5574399043799ea8059afbbc
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-05-05 02:16:54 -0700

    create/use middleware complete signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,48 @@
-import { Router } from 'express';
+import jwt from 'jsonwebtoken';
+import { Router, Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const router = Router();
 
-router.get('/api/users/signin', (req, res) => {
-  res.send('signin!!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password must be supplied')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new BadRequestError('Invalid credential');
+    }
+
+    const isValid = await Password.compare(user.password, password);
+    if (!isValid) {
+      throw new BadRequestError('Invalid credential');
+    }
+
+    console.log(user);
+
+    const userJwt = jwt.sign(
+      {
+        id: user._id,
+        email: user.email
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = { jwt: userJwt };
+
+    res.status(200).send(user);
+  }
+);
 
 export { router as signinRouter };
