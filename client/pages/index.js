09d29c680ec8c575fbedbd40f4ae1ca162a32386
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-05-06 17:43:02 -0700

    make signin

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const index = ({ currentUser }) => {
-  console.log(currentUser);
-  return <div>Landing Page</div>;
+  return <h1>You are {currentUser ? '' : 'not '}signed in</h1>;
 };
 
-index.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers
-      }
-    );
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
+index.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
 };
 
 export default index;
