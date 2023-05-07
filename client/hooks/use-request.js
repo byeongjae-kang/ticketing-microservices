b1cfa1be21d6df6fea809a2997013392f43a5789
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-05-06 17:22:09 -0700

    setup index and signup page

---
+++ client/hooks/use-request.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const useRequest = ({ url, method, body, onSuccess, onFailure }) => {
+  const [errors, setErrors] = useState(null);
+
+  const request = async () => {
+    try {
+      setErrors(null);
+
+      const response = await axios[method](url, body);
+      onSuccess?.(response.data);
+
+      return response.data;
+    } catch (err) {
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Oops.....</h4>
+          <ul>
+            {err.response.data.errors.map((error) => (
+              <li key={error.message}>{error.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+      onFailure?.();
+    }
+  };
+
+  return { errors, request };
+};
+
+export default useRequest;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
-const index = () => {
-  return <div>index</div>;
+const index = ({ currentUser }) => {
+  console.log(currentUser);
+  return <div>Landing Page</div>;
+};
+
+index.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers
+      }
+    );
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
 };
 
 export default index;
