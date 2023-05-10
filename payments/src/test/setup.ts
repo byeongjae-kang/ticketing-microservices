f832699af271ce8042e0bbe2c6e08110d4f4de2b
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-05-09 18:58:18 -0700

    basic setup

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,47 @@
+import mongoose, { Types } from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import jwt from 'jsonwebtoken';
+
+let mongo: MongoMemoryServer;
+
+jest.mock('../nats-wrapper');
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'secret key';
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri, { dbName: 'verifyMASTER' });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  collections.forEach((collection) => collection.deleteMany({}));
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.disconnect();
+});
+
+declare global {
+  var signin: () => string[];
+}
+
+global.signin = () => {
+  const payload = {
+    id: new Types.ObjectId(),
+    email: 'test@test.com'
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  return [`session=${base64}`];
+};
